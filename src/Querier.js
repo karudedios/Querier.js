7a4518b297c12c7ae3ec056775b50411317adac9
karudedios <c.dedios@outlook.com>
2015-09-03 17:59:17 -0400

    initial commit, just so I can work from home with this

---
+++ src/Querier.js
@@ -0,0 +1,70 @@
+export default (() => {
+  Object.prototype.put = function(key, value) {
+    this[key] = value;
+    return this;
+  }
+
+  Object.prototype.putWhen = function(predicate, key, value) {
+    return predicate
+      ? this.put(key, value)
+      : this;
+  }
+
+  String.prototype.format = function(...args) {
+    return this.replace(/\{(\d)\}/g, (_, $1) => args[+$1]);
+  }
+
+  let getArgs = (f) => f.toString().match(/\((.*)\)\W?{/)[1].replace(/ /g, '');
+  let getBody = (value) => value instanceof Function ? `(${getArgs(value)})` : '';
+  let getFormat = (body, action, k) => `$q['${k}']${body}.${action}(function(${k}) { return {0}; })`
+
+  class Querier {
+    constructor(dictionary) {
+      this.dictionary = dictionary || {};
+    }
+
+    pipeMany(...pipes){
+      return pipes.reduce((query, pipe) => query.pipe(pipe), this);
+    }
+
+    pipe({ as, from, where }){
+      let id = Object.keys(this.dictionary).filter((k) => k.indexOf('_') > -1).length;
+      let _from = `_${id + 1}`;
+
+      return new Querier(this.dictionary.put(as, from).putWhen(where != undefined, _from, where));
+    }
+
+    from(name){
+      return {
+        in: (selectable) => {
+          return new Querier(this.dictionary.put(name, selectable));
+        }
+      }
+    }
+
+    select(cb){
+      let $q = this.dictionary;
+      let keys = Object.keys($q);
+      
+      if (keys.length == 0) return;
+
+      let constructor = $q[keys[0]].constructor.__proto__;
+
+      if (!keys
+          .filter(t => $q[t].constructor.name != 'Function')
+          .map(t => $q[t])
+          .every((c) => c instanceof constructor)) {
+        throw "Every enumerated object must be of the same type";
+      }
+
+      let getAction = (orig, i) => orig.length === (i + 1) ? 'select' : 'selectMany';
+      let resolveReduce = (str, k, i, orig) => str.format(getFormat(getBody($q[k]), getAction(orig, i), k));
+
+      let ac = keys.reduce(resolveReduce, '{0};').format(`cb(${ getArgs(cb) })`)
+
+      return eval(ac);
+    }
+  }
+
+  return { new(init) { return new Querier(init || {}) } };
+})();
