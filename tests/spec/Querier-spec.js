261a1325b8de868d330017154e544c6899949a4c
Carlos de Dios <c.dedios@outlook.com>
2015-09-06 02:34:03 -0400

    Adjusting spec to changes

--- tests/spec/Querier-spec.js
+++ tests/spec/Querier-spec.js
@@ -3,19 +3,22 @@ import { Querier } from '../../src/Querier'
 
 describe("Querier", () => {
 
-  describe("from/in", () => {
+  describe("pipe", () => {
     let list = new NodeList(1, new NodeList(2, new NodeList(3, new EmptyList)));
 
     it("should allow you to do a selection from a type that supports the 'select' Method", () => {
-      let result = Querier.build().from("item").in(list).select((item) => item + 2);
+      let result = Querier.build()
+        .pipe({ as: 'item', from: list })
+        .select((item) => item + 2);
+
       expect(result).toEqual(list.select(x => x + 2));
     });
 
     it("shoud allow you to do a selection based on multiple from's in a type that supports 'selectMany'", () => {
       let result = Querier.build()
-        .from("a").in(list)
-        .from("b").in(list)
-        .from("c").in(list)
+        .pipe({ as: 'a', from: list })
+        .pipe({ as: 'b', from: list })
+        .pipe({ as: 'c', from: list })
         .select((a, b, c) => a + b + c);
 
       let whatShouldBe = list.selectMany(a => list.selectMany(b => list.select(c => a + b + c)));
@@ -23,18 +26,20 @@ describe("Querier", () => {
     });
 
     it("should allow you to specify a 'where'", () => {
-      /**
-       * should be : $q['a'].where($q['_1']).select(a => a)
-       * instead of: $q['a'].selectMany(function(a) { return $q['_1'](a).select(function(_1) { return cb(a); }); });
-       */
-
       let result = Querier.build()
-        .from('a').in(list)
-        .where((a) => a % 2 === 0)
+        .pipe({ as: 'a', from: list, where: (a) => a % 2 === 0 })
         .select((a) => a);
 
+      expect(result).toEqual(list.where((h)=> h % 2 === 0));
+    });
 
-      expect(result).toEqual(list.where((h)=> h % 2 === 0))
+    it("should work with multiple clauses", () => {
+      let result = Querier.build()
+        .pipe({ as: 'a', from: list, where: (a) => a % 2 == 0 })
+        .pipe({ as: 'b', from: list, where: (b) => b % 3 == 0 })
+        .select((a, b) => a + b);
+
+      expect(result).toEqual(list.where(x => x % 2 === 0).selectMany(a => list.where(x => x % 3 === 0).select(b => a + b)));
     });
   });
 });
